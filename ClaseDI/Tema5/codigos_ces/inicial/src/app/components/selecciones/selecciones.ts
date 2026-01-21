b2518cd80ada95b77187df66877a09fd4b6d87ec
danielzdr <danielzuniga20032000@gmail.com>
2026-01-21 14:09:12 +0100

    update

--- ClaseDI/Tema5/codigos_ces/inicial/src/app/components/selecciones/selecciones.ts
+++ ClaseDI/Tema5/codigos_ces/inicial/src/app/components/selecciones/selecciones.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import Swal from 'sweetalert2';
 
 @Component({
   selector: 'app-selecciones',
@@ -7,7 +8,60 @@ import { Component } from '@angular/core';
   styleUrl: './selecciones.css',
 })
 export class Selecciones {
-  seleccionTipo=1;
-  
-  
+  nombre='';
+  direccionEnvio='';
+  direccionFacturacion='';
+  coste=0;
+  tipoPago='';
+  numeroTarjeta=0;
+  caducidad='';
+  cvv=0;
+  numeroTelefono=0;
+  iban='';
+  analizada=false;
+  resolucion=false;
+
+
+  validarCompra(){
+    if(this.nombre && this.direccionEnvio && this.direccionFacturacion && this.coste && this.tipoPago){
+      if(this.tipoPago){
+         Swal.fire({
+      title: "Pago corretamente realizado",
+      icon: "success",
+      draggable: true
+    
+        });
+      }else{
+        this.resolucion=false
+      }
+      this.analizada=true;
+        setTimeout(() => {
+          this.resetearDatos();
+        }, 5000);
+   
+    }else{
+    Swal.fire({
+    icon: "error",
+    title: "Error",
+    text: "pago no realizado, faltan datos",
+    draggable: true
+});
+    }
+  }
+
+  resetearDatos(){
+    this.nombre=''
+    this.direccionEnvio=''
+    this.direccionFacturacion=''
+    this.coste=0
+    this.tipoPago=''
+    this.numeroTarjeta=0;
+    this.caducidad='';
+    this.cvv=0;
+    this.numeroTelefono=0;
+    this.iban='';
+    this.analizada=false;
+
+
+  }
 }
